3676032bb30d3625c2a5f7e6162e2f5d89d34a4a
Yuhanun Citgez <y.citgez@student.utwente.nl>
2020-04-19 17:28:35 +0200

    Tests

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 pub use tokenizer::tokens::Tokenizer;
 
 fn main() {
-    let mut tokenizer = Tokenizer::new("let data: int = 5;".to_owned());
-    let tokens = tokenizer.into_tokens();
-    println!("{:#?}", tokens);
-    println!("-------");
+    // let mut tokenizer = Tokenizer::new("let data: int = 5;".to_owned());
+    // let tokens = tokenizer.into_tokens();
+    // println!("{:#?}", tokens);
+    // println!("-------");
 
     let mut tokenizer = Tokenizer::new("\"hello world!\"".to_owned());
     let tokens = tokenizer.into_tokens();
---
+++ tests/tokenizer_basic.rs
@@ -0,0 +1,13 @@
+#[cfg(test)]
+mod tokenizer_tests {
+    #[test]
+    fn basic_string_test() {
+        let mut tokenizer = tokenizer::tokens::Tokenizer::new(";;;\"hello world!\";;;".to_owned());
+        let tokens = tokenizer.into_tokens();
+        let tokens: Vec<&str> = tokens.iter().map(|elem| &elem[..]).collect();
+        assert!(match tokens.as_slice() {
+            [";", ";", ";", "\"hello world!\"", ";", ";", ";"] => Ok(()),
+            _ => Err(())
+        }.is_ok())
+    }
+}
